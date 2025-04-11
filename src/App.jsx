2beb893619fc487a117ddb32825f88f73f0bf0ac
Maverick <maverickseneris@gmail.com>
2025-04-11 11:19:31 +0800

    feat: delete function

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,16 @@ import NoteList from "./components/noteList";
 
 function App() { 
   const [notes, setNotes] = useState([]);
+  
+  function deleteNote(noteToDel){ 
+    setNotes(prevNotes => prevNotes.filter(note => note.name !== noteToDel))
+  }
   return (
     <div className="m-4 flex flex-col justify-center items-center">
       <h1 className="mb-4 font-bold text-3xl">QuickNotes</h1>
       <div className="flex flex-col gap-2">
         <FormInput notes={notes} setNotes={setNotes} />
-        <NoteList notes={notes} />
+        <NoteList notes={notes} deleteNote={deleteNote}/>
       </div>
     </div>
   );
--- src/components/noteList.jsx
+++ src/components/noteList.jsx
@@ -1,11 +1,11 @@
-function NoteList({notes}) {
+function NoteList({notes, deleteNote}) {
     return (
         <div>
             <span>
                 {notes.map((note, index) => { 
                     return (
                         <div key={index}>
-                            <span>- {note.name}</span>
+                            <span>- {note.name} <button onClick={()=> deleteNote(note.name)}>delete</button></span>
                         </div>
                     )
                 })}
